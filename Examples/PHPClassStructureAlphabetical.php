39c19504e7417a34557ba2e47cd5118659a75645
Navarr Barnier <navarr@mediotype.com>
2021-03-31 16:24:26 -0400

    Added Check for correctly ordered methods

---
+++ Examples/PHPClassStructureAlphabetical.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+namespace Mediotype\Module\Framework;
+
+class PHPClassStructureAlphabetical
+{
+    public function __construct()
+    {
+    }
+
+    protected function _construct()
+    {
+    }
+
+    protected function _init()
+    {
+    }
+
+    public function a()
+    {
+    }
+
+    public function b()
+    {
+    }
+
+    public function c()
+    {
+    }
+
+    public function d()
+    {
+    }
+
+    protected function e()
+    {
+    }
+
+    protected function f()
+    {
+    }
+
+    protected function g()
+    {
+    }
+
+    protected function h()
+    {
+    }
+
+    private function i()
+    {
+    }
+
+    private function j()
+    {
+    }
+
+    private function k()
+    {
+    }
+
+    private function l()
+    {
+    }
+}
